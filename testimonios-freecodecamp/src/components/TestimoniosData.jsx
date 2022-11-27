b5cc656ca2d3d2f3aa5f50ce5f2976fc344fd8e9
davidlp90 <davidivan.eda@gmail.com>
2022-11-27 20:39:29 -0300

    Update TestimoniosData.jsx

--- testimonios-freecodecamp/src/components/TestimoniosData.jsx
+++ testimonios-freecodecamp/src/components/TestimoniosData.jsx
@@ -16,6 +16,8 @@
 //     "It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life.",
 // };
 
+//funcion para crear nuevos testimonios
+
 function testimonioData(nombre, pais, imagen, cargo, empresa, testimonio) {
   this.nombre = nombre;
   this.pais = pais;
