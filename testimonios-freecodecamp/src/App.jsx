cc3ca0233f106c779f1cf2fd7b750ced16da14e1
davidlp90 <davidivan.eda@gmail.com>
2022-11-28 04:52:35 -0300

    Updated with dynamic created components

--- testimonios-freecodecamp/src/App.jsx
+++ testimonios-freecodecamp/src/App.jsx
@@ -1,18 +1,29 @@
 import "./App.css";
 import Testimonio from "./components/Testimonio.jsx";
-import {
-  testimonioDataOne,
-  testimonioDataTwo,
-  testimonioDataThree,
-} from "./components/TestimoniosData.jsx";
+// import {
+//   testimonioDataOne,
+//   testimonioDataTwo,
+//   testimonioDataThree,
+// } from "./components/TestimoniosData.jsx";
+import { testimonios } from "./testimonios";
 function App() {
   return (
     <div className="App">
       <div className="contenedor-principal">
         <h1>Here is what our alumni say about freeCodeCamp:</h1>
-        <Testimonio {...testimonioDataOne} />
+        {testimonios.map((testimonios) => (
+          <Testimonio
+            nombre={testimonios.nombre}
+            pais={testimonios.pais}
+            imagen={testimonios.imagen}
+            cargo={testimonios.cargo}
+            empresa={testimonios.empresa}
+            testimonio={testimonios.testimonio}
+          />
+        ))}
+        {/* <Testimonio {...testimonioDataOne} />
         <Testimonio {...testimonioDataTwo} />
-        <Testimonio {...testimonioDataThree} />
+        <Testimonio {...testimonioDataThree} /> */}
       </div>
     </div>
   );
